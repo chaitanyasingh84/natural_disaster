fe582f582f51f7cde551a074cd30f0996add6d21
jordanYChu <jordan.yn.chu@gmail.com>
2024-10-26 19:32:15 -0400

    updated client logic

--- clientLogic.js
+++ clientLogic.js
@@ -14,7 +14,6 @@ function haversineDistance(lat1, lon1, lat2, lon2) {
 
 function filterLocationsWithinRadius(targetLat, targetLon, stations, radius) {
     available_locations = {}
-    console.log("all makrers", markers)
     for(stationName in stations) {
         const distance = haversineDistance(targetLat, targetLon, stations[stationName]["lat"], stations[stationName]["lon"]);
         if(distance <= radius) {
@@ -25,7 +24,10 @@ function filterLocationsWithinRadius(targetLat, targetLon, stations, radius) {
             //     commodity,
             //     newQuantity
             // );
-            available_locations[station] = stations[station]
+            available_locations[stationName] = stations[stationName]
+        }
+        else {
+            removeMarker(stationName)
         }
     }
     return available_locations
@@ -36,7 +38,24 @@ const targetLat = 2; // Target latitude
 const targetLon = 2; // Target longitude
 const radius = 160; // 
 
+async function removeMarker(stationName) {
+    // Check if the marker exists in the markers object
+    console.log("Test")
+    for (let key in markers) {
+        console.log(`Key: "${key}"`); // Logs each key to see if any oddities are present
+    }
+    const marker = await markers[stationName]
+    if (marker) {
+        map.removeLayer(markers[stationName]); // Remove the marker from the map
+        delete markers[stationName]; // Remove the marker from the markers object
+        console.log(`Marker for station '${stationName}' removed.`);
+    } else {
+        console.log(`No marker found for station '${stationName}'.`);
+    }
+}
 
+console.log(markers)
+removeMarker("TestingStation")
 
-console.log(filterLocationsWithinRadius(targetLat,targetLon,stations,radius), "test")
+// console.log(filterLocationsWithinRadius(targetLat,targetLon,stations,radius), "test")
 updateStationList()
