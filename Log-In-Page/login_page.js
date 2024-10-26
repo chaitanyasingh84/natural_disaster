6dde83c5a7750642d71356aa50008d219204be0c
Mathuran-S <121098382+Mathuran-S@users.noreply.github.com>
2024-10-26 17:24:58 -0400

    Updated window.location for admin user

--- Log-In-Page/login_page.js
+++ Log-In-Page/login_page.js
@@ -6,10 +6,10 @@ const adminUserRB = document.getElementById("admin");
 function redirect(){
     if(publicUserRB.checked){
         console.log("public user");
-        window.location.assign('../index.html');
     }
     else if(adminUserRB.checked){
         console.log("admin user");
+        window.location.assign('../index.html');
         //window.location.assign('../dashboard.html')
     }
 }
