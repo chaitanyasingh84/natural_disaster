2b43b22e4318ac1b063fb6633472a6f52de53b77
Warrick Tsui <warricktsui1226@gmail.com>
2024-10-27 03:40:08 -0400

    More database changes

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getDatabase, push, ref, set , get } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+import { getDatabase, push, ref , get , child } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 const firebaseConfig = {
   apiKey: "AIzaSyACx61Met7FUTN9yIK-YhNzQLl6iI-Lzx0",
   authDomain: "newhacksnaturaldisaster.firebaseapp.com",
@@ -16,11 +16,24 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 
 function addDatabaseStation(name, lat, long, commodities){
-  set(ref(db, name), {
-    Latitude: lat,
-    Longitude: long,
-    Commodities: {},
+    var stationsReference = ref(db, 'Deployment Stations/');
+  stationsReference.child("name").setValue(name)
+
+  // var stationsReference = ref(db, 'Deployment Stations/');
+  // stationsReference.child(name).setValue(
+  //   lat
+  // )
+  /*
+  
+  stationsReference.child("name").setValue()
+  push(stationsReference, {
+    StationName: name,
+    Properties:{
+        Latitude: lat,
+        Longitude: long,
+    }
   })
+    */
   .then(()=>{
     alert("Deployer Station Added Successfully");
   })
@@ -32,7 +45,7 @@ function addDatabaseStation(name, lat, long, commodities){
 }
 
 function addCommodityToStation(deployerStation, commodityType, commodityQuantity){
-  var commoditiesReference = ref(db, deployerStation + '/Commodities');
+  var commoditiesReference = ref(db, 'Deployment Stations/' + deployerStation + '/Commodities');
   push(commoditiesReference, {
       type: commodityType,
       quantity: commodityQuantity
@@ -46,23 +59,24 @@ function addCommodityToStation(deployerStation, commodityType, commodityQuantity
   })
 }
 
-
-
-  /*
-  else{
-    push(commodityList, commodityType)
-  .then(()=>{
-    alert("Commodity Added Successfully");
-  })
-  .catch((error) =>{
-    alert("An error has occurred!");
-    console.log(error);
-  })
-
-  }*/
   async function addNewCommodity(commodityType){
     //Check if the commodity already exists. 
     const dbRef = ref(db, 'CommodityTypes');
+
+    push(dbRef, commodityType)
+    .then(()=>{
+      alert("Commodity Added Successfully");
+    })
+    .catch((error) =>{
+      alert("An error has occurred!");
+      console.log(error);
+    })
+
+  }
+
+  async function readDeploymentStation(commodityType){
+    //Check if the commodity already exists. 
+    const dbRef = ref(db, 'Deployment Stations/');
     get(dbRef)
     .then((snapshot) => {
       if (snapshot.exists()) {
@@ -73,59 +87,9 @@ function addCommodityToStation(deployerStation, commodityType, commodityQuantity
     })
     .catch((error) => {
       console.error(error);
-    });
+    });}
 
-// dbRef.on(j
-    //   snapshot.forEach((childSnapshot) => {
-    //     const childKey = childSnapshot.key;
-    //     const childData = childSnapshot.val();
-    //     // ...
-    //   });
-    // }, {
-    //   onlyOnce: true
-    // });
 
-    
-    // $("div").on("click", function() {
-    //   console.log("commodasdasdity")
-    // })
-    // var ref = ref(db, 'CommodityTypes');
-    
-    // ref.once('value', function(snapshot) {
-    //   snapshot.forEach(function(childSnapshot) {
-    //     var childKey = childSnapshot.key;
-    //     var childData = childSnapshot.val();
-    //     $scope.foo[childKey] = childData;
-    //   });
-    // });
-    // commodityList.on('value', (snapshot) => {
-    //   snapshot.forEach((child) => {
-    //     console.log(child.key); // "child1", "child2"
-    //     console.log(child.child("data").val()); // "123", "123"
-    //   })});
-    // var commodityList = ref(db, 'CommodityTypes');
-    // var commodityExists = false;
-    // console.log(commodityList)
-    // commodityList.once('value').then(snapshot =>{
-    //   if(snapshot.exists()){
-    //     console.log('Element Exists!');
-    //   }
-    //   else{
-    //     console.log('DNE');
-    //   }
-    // })
-}
-
-function commodityExists(data){
-  let commodityTypes = data.val();
-  let keys = Object.keys(commodityTypes);
-
-  for(let i = 0; i < keys.length; i++){
-    let infoData = keys[i].val();
-
-  }
-
-}
 function deleteDatabaseStation(deployerStation){}
 
 //function edit Commodity
